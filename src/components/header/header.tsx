d35e1cacc481a7cb3d252425cdf44976602b4196
jtpark <vigabhan1231@gmail.com>
2022-10-01 15:01:36 +0900

    style: mobile menu 퍼블
    tablet 사이즈부터 적용되는 mobile header menu에 대한 기본 퍼블 작업.

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,12 @@
 import { useRouter } from 'next/router';
-import { MouseEvent } from 'react';
-import { useMedia } from 'react-use';
+import { MouseEvent, useEffect } from 'react';
+import { useMedia, useToggle } from 'react-use';
+import MobileMenu from './mobile-menu';
 import PcMenu from './pc-menu';
 
 function Header() {
   const router = useRouter();
+  const [isMenuOpen, setIsMenuOpen] = useToggle(false);
   const isWide = useMedia('(min-width: 1000px)', false);
 
   const handleLogoClick = (e: MouseEvent<HTMLButtonElement>) => {
@@ -12,17 +14,30 @@ function Header() {
     router.push('/');
   };
 
+  useEffect(() => {
+    if (isWide) {
+      setIsMenuOpen(false);
+    }
+  }, [isWide]);
+
   return (
-    <header className="header">
-      <button className="header__logo" onClick={handleLogoClick} />
-      {isWide ? (
-        <PcMenu />
-      ) : (
-        <button className="header__menu-btn">
-          <i className="fas fa-bars" />
-        </button>
-      )}
-    </header>
+    <>
+      <header className="header">
+        <button className="header__logo" onClick={handleLogoClick} />
+        {isWide ? (
+          <PcMenu />
+        ) : (
+          <button className="header__menu-btn" onClick={setIsMenuOpen}>
+            {isMenuOpen ? (
+              <i className="fas fa-times" />
+            ) : (
+              <i className="fas fa-bars" />
+            )}
+          </button>
+        )}
+      </header>
+      {isMenuOpen && <MobileMenu />}
+    </>
   );
 }
 
--- src/components/header/mobile-menu/mobile-menu.tsx
+++ src/components/header/mobile-menu/mobile-menu.tsx
@@ -1,30 +1,117 @@
-import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { MouseEvent } from 'react';
+import BlurImage from '../../blur-image';
 
 function MobileMenu() {
+  const router = useRouter();
+
+  const onMenuItemClick = (event: MouseEvent<HTMLLIElement>) => {
+    router.push(event.currentTarget.dataset.url as string);
+  };
+
   return (
     <nav className="mobile-menu__nav__1st">
       <ul className="mobile-menu__nav__1st-list">
         <li>
-          MODEL
+          <button>
+            Model
+            <i className="fas fa-chevron-down active" />
+          </button>
           <ul className="mobile-menu__nav__2nd">
-            <li>VESPA</li>
-            <li>PIAGGIO</li>
+            <li>
+              <button>- VESPA</button>
+              <ul className="mobile-menu__nav__3rd">
+                <li>
+                  <button>
+                    PRIMAVERA
+                    <i className="fas fa-chevron-down active" />
+                  </button>
+                  <div className="mobile-menu__nav__4th">
+                    <ul>
+                      <li>
+                        <BlurImage
+                          src="https://vespa-bucket.s3.ap-northeast-2.amazonaws.com/model/3a83e8e9-5635-40ea-ad95-9b7879fb484d_%ED%94%84%EB%A6%AC%EB%A7%88-%EC%8A%A4%ED%83%A0%EB%8B%A4%EB%93%9C.jpg"
+                          alt="primavera"
+                          remote
+                        />
+                        <p>PRIMAVERA</p>
+                      </li>
+                      <li>
+                        <BlurImage
+                          src="https://vespa-bucket.s3.ap-northeast-2.amazonaws.com/model/3a83e8e9-5635-40ea-ad95-9b7879fb484d_%ED%94%84%EB%A6%AC%EB%A7%88-%EC%8A%A4%ED%83%A0%EB%8B%A4%EB%93%9C.jpg"
+                          alt="primavera"
+                          remote
+                        />
+                        <p>S Version</p>
+                      </li>
+                      <li>
+                        <BlurImage
+                          src="https://vespa-bucket.s3.ap-northeast-2.amazonaws.com/model/3a83e8e9-5635-40ea-ad95-9b7879fb484d_%ED%94%84%EB%A6%AC%EB%A7%88-%EC%8A%A4%ED%83%A0%EB%8B%A4%EB%93%9C.jpg"
+                          alt="primavera"
+                          remote
+                        />
+                        <p>PRODUCT(RED)</p>
+                      </li>
+                      <li>
+                        <BlurImage
+                          src="https://vespa-bucket.s3.ap-northeast-2.amazonaws.com/model/3a83e8e9-5635-40ea-ad95-9b7879fb484d_%ED%94%84%EB%A6%AC%EB%A7%88-%EC%8A%A4%ED%83%A0%EB%8B%A4%EB%93%9C.jpg"
+                          alt="primavera"
+                          remote
+                        />
+                        <p>PIC-NIC</p>
+                      </li>
+                      <li>
+                        <BlurImage
+                          src="https://vespa-bucket.s3.ap-northeast-2.amazonaws.com/model/3a83e8e9-5635-40ea-ad95-9b7879fb484d_%ED%94%84%EB%A6%AC%EB%A7%88-%EC%8A%A4%ED%83%A0%EB%8B%A4%EB%93%9C.jpg"
+                          alt="primavera"
+                          remote
+                        />
+                        <p>75TH Anniverary</p>
+                      </li>
+                    </ul>
+                  </div>
+                </li>
+                <li>
+                  <button>
+                    SPRINT
+                    <i className="fas fa-chevron-down active" />
+                  </button>
+                </li>
+                <li>
+                  <button>
+                    GTS
+                    <i className="fas fa-chevron-down active" />
+                  </button>
+                </li>
+                <li>
+                  <button>
+                    GTV
+                    <i className="fas fa-chevron-down active" />
+                  </button>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <button>- PIAGGIO</button>
+            </li>
           </ul>
         </li>
         <li>
-          ACCESSORIES&PARTS
+          <button>Accessories&Parts</button>
           <ul className="mobile-menu__nav__2nd">
-            <li>정품 악세사리</li>
+            <li>
+              <button>- 정품 악세사리</button>
+            </li>
           </ul>
         </li>
-        <li>
-          <Link href="/promotion">PROMOTION</Link>
+        <li data-url="/promotion" onClick={onMenuItemClick}>
+          <button>Promotion</button>
         </li>
-        <li>
-          <Link href="/service">SERVICE</Link>
+        <li data-url="/service" onClick={onMenuItemClick}>
+          <button>Service</button>
         </li>
-        <li>
-          <Link href="/link/driver">시승신청</Link>
+        <li data-url="/link/driver" onClick={onMenuItemClick}>
+          <button>시승신청</button>
         </li>
       </ul>
     </nav>
